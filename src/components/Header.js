093fb7dec6649dcaf00da2a3296927632a8d768f
Jon-Stickrod <jonstickrodCodes@gmail.com>
2020-05-26 20:22:11 -0700

    not done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = props => {
   return (
     <>
       <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+        <img src={props.cars.image} alt={props.cars.name} />
       </figure>
       <h2>{props.car.name}</h2>
       <p>Amount: ${props.car.price}</p>
